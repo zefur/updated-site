44bbfe4342bb2e9dfc698b5093d12f2ad6e22304
James Hall <jdhall@live.co.uk>
2020-01-24 21:34:22 +0800

    added basic blog functionality and comments system

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,32 @@
 class BlogsController < ApplicationController
     def index
+        @blogs = Blog.all
     end
-    def create
+    def show
+        @blog = Blog.find(params[:id])
     end
-    def new
+   
+def create
+    @blog = Blog.new(blog_params)
+   
+    if @blog.save
+      redirect_to @blog
+    else
+      render 'new'
     end
-
+  end
+   
+  def update
+    @blog = Blog.find(params[:id])
+   
+    if @blog.update(blog_params)
+      redirect_to @blog
+    else
+      render 'edit'
+    end
+  end
+    private
+    def blog_params
+        params.require(:blog).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
 
   root 'pages#landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
